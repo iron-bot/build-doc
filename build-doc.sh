bcaab4a4f8ad87174754e4306d9859b3b37af151
zzmp <zmp@umich.edu>
2014-08-22 21:14:16 -0700

    (fix) pushd->cd.

--- build-doc.sh
+++ build-doc.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
   curl https://raw.githubusercontent.com/iron/iron.github.io/master/header.html > header.html
   curl https://raw.githubusercontent.com/iron/iron.github.io/master/footer.html > footer.html
   rustdoc -L ./target/deps -o ./docs/doc src/lib.rs --html-in-header ./head.html --html-before-content ./header.html --html-after-content ./footer.html
-  pushd docs && git add --all
+  cd docs && git add --all
   git config user.name "iron-bot"
   git config user.email "ironframework@gmail.com"
   git commit -m "(docs-autogen) ${TRAVIS_REPO_SLUG}."
