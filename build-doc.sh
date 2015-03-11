a956b230cec87e7d78568d0131a106c515dcd451
Zach Pomerantz <zmp@umich.edu>
2015-03-10 23:27:07 -0700

    (fix) Specify liblog name

--- build-doc.sh
+++ build-doc.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
   curl https://raw.githubusercontent.com/iron/iron.github.io/master/header.html > header.html
   curl https://raw.githubusercontent.com/iron/iron.github.io/master/footer.html > footer.html
   # Log is included in the toolchain for rustdoc, so we must override it to use our own
-  ls target/debug/deps/*log* | xargs -I % rustdoc -L ./target/debug/deps --extern log=% -o ./docs/doc src/lib.rs --html-in-header ./head.html --html-before-content ./header.html --html-after-content ./footer.html
+  ls target/debug/deps/liblog-*.rlib | xargs -I % rustdoc -L ./target/debug/deps --extern log=% -o ./docs/doc src/lib.rs --html-in-header ./head.html --html-before-content ./header.html --html-after-content ./footer.html
   cd docs && git add --all
   git config user.name "iron-bot"
   git config user.email "ironframework@gmail.com"
