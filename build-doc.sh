93746873665ef804e0864718ff1ee4a3b47f5f60
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-04-14 16:40:03 +0200

    Use docs.rs

--- build-doc.sh
+++ build-doc.sh
@@ -1,22 +1,19 @@
+#!/bin/bash
+set -xe
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_RUST_VERSION" = 'nightly' ]; then
   # Fetch the docs
   git clone https://github.com/iron/iron.github.io.git docs
-  
-  # TODO: Custom stylings
 
-  # Doc the crate and its dependencies
-  cargo doc --all-features
-  mkdir tmp
-  mv target/doc/search-index.js new-search-index.js
-  cp docs/doc/search-index.js old-search-index.js
-  # Update the docs
-  cp -Rf target/doc/* docs/doc
-  # Update the search indices
-  curl -L -O https://raw.githubusercontent.com/iron/build-doc/master/merge-search-indices/target/release/merge-search-indices
-  chmod +x merge-search-indices
-  ./merge-search-indices && mv merged-search-index.js docs/doc/search-index.js
+  DOC_NAME="${TRAVIS_REPO_SLUG##*/}"
 
-  cd docs && git add --all
+  cd docs/doc/
+  rm -rf $DOC_NAME
+  mkdir $DOC_NAME
+  cd $DOC_NAME
+  echo "<meta http-equiv=refresh content='3; URL=https://docs.rs/$DOC_NAME'>" > index.html
+
+  add --all
   git config user.name "iron-bot"
   git config user.email "ironframework@gmail.com"
   git commit -m "(docs-autogen) ${TRAVIS_REPO_SLUG}."
